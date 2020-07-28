518531af55b9a9ecfae17209c1f5bd4b33a070b1
Tommy Anfosso <anfossotommy@hotmail.fr>
2020-07-28 16:21:21 +0200

    reverse smiley orientation in frag shader

--- fs/frag.fs
+++ fs/frag.fs
@@ -2,12 +2,13 @@
 
 out vec4 FragColor;
 in vec2 TexCoord;
+in vec2 FaceTexCoord;
 uniform vec3 ourColor;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 
 void main()
 {
-	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2) * vec4(ourColor, 1.0);
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, FaceTexCoord), 0.2) * vec4(ourColor, 1.0);
 	//FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 }
--- vs/vert.vs
+++ vs/vert.vs
@@ -3,9 +3,11 @@
 layout(location = 0) in vec3 aPos;
 layout(location = 2) in vec2 aTexCoord;
 out vec2 TexCoord;
+out vec2 FaceTexCoord;
 
 void main()
 {
 	gl_Position = vec4(aPos, 1.0);
 	TexCoord = aTexCoord;
+	FaceTexCoord = vec2(-aTexCoord.x, aTexCoord.y);
 }
